3057f9a3135e1f77d97a18cc269028e61fceec79
Bobuli <Benni.Bischke@gmx.de>
2011-02-11 22:53:01 +0100

    Inital Checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Igni::Application.config.secret_token = 'fd8c3bc65f7b10af05d65686dce65f7de3bbad7046c899f704330f49016edb9c4008490b57adb6d43f339315d3cf09f6d6c4bafecefa37e7127780fd036f3999'
